9b7910552f0d891ae23d279f8900d8b59ebd82b0
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-15 22:03:42 -0300

    Blog. Unit tests. Step 2. Test favoriteBlog funtion. Passed (4.5*)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,21 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    let favorite = blogs[0]
+    for (let blog of blogs) {
+        if (blog.likes > favorite.likes) {
+            favorite = blog
+        }
+    }
+    return favorite
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
