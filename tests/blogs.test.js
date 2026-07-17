1cb24a2373c3953ee7e4f1502d5a31f51e9a08d1
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-17 00:04:28 -0300

    Blogs. Step 3. Test POST method (4.10)

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,4 +1,5 @@
 const { test, after, describe } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -14,10 +15,38 @@ describe('BLOGS: GET endpoint tests', () => {
 
     test('unique identifier property of the blog posts is named id', async () => {
         const response = await api.get('/api/blogs')
+        console.log("BODY ", response.body)
         if (response.body.length > 0) {
-            expect(response.body[0].id).toBeDefined()
-            expect(response.body[0]._id).not.toBeDefined()
+            assert.ok(response.body[0].id) // id identifier is OK
+            assert.strictEqual(response.body[0]._id, undefined) // _id identifier is not define
         }
+    })
+})
+
+describe('BLOGS: POST endpoint tests', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: 'Entorno de prueba',
+            author: 'Matti Luukkainen',
+            url: 'https://fullstackopen.com/es/part4/probando_el_backend#entorno-de-prueba',
+            likes: 5,
+        }
+        // Before POST
+        const initialResponse = await api.get('/api/blogs')
+        const initialCount = initialResponse.body.length
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        // After POST
+        const finalResponse = await api.get('/api/blogs')
+        assert.strictEqual(finalResponse.body.length, initialCount + 1)
+
+        const titles = finalResponse.body.map(r => r.title)
+        assert.ok(titles.includes('async/await'))
     })
 })
 
